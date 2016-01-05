c81a84f0cf038c5ba3ea4c7f469e0bd61e51edc4
Dennis Schroer <dennisschroer@hotmail.com>
2016-01-05 16:13:35 +0100

    VMs in server, World node, MigrationPolicy interface; added cpu usage and paths to VM

--- src/main/java/vm/VM.java
+++ src/main/java/vm/VM.java
@@ -1,7 +1,12 @@
 package vm;
 
+import cluster.Connection;
+import cluster.Path;
 import lombok.Data;
 import lombok.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // Each VM type is characterized by processing performance defined in MIPS, RAM
 // capacity, storage capacity and network bandwidth
@@ -19,4 +24,11 @@ public class VM {
      * The maximum bandwidth of the internet connection, in Mbps.
      */
     @NonNull private int maxBandwidth;
+
+    private int CPU;
+    private List<Path> paths = new ArrayList<Path>();
+
+    public int getCPU() {
+        return CPU;
+    }
 }
