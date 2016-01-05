c0e8a9878db7fc0bc6585894b2f6e48c9567a2d5
Peter van Dijk <petervandijk1991@gmail.com>
2016-01-05 15:22:53 +0100

    Added values

--- src/main/java/cluster/Server.java
+++ src/main/java/cluster/Server.java
@@ -6,4 +6,26 @@ import lombok.Data;
 @Data
 public class Server extends Node {
 
+
+    /**
+     * Based on:
+     *
+     * FIVE WAYS TO REDUCE DATA CENTER SERVER POWER CONSUMPTION
+     * Blackburn 2008
+     */
+    public static final int MAX_POWER = 300;
+    public static final int MIN_POWER = 200;
+
+    /**Maximum possible CPU load in MIPS*/
+    public static final int MAX_CPU = 0;
+
+    /**
+     * Total MIPS done now
+     * @invariant 0<= CPULoad <= MAX_CPU
+     */
+    private double CPULoad = 0;
+
+    public double getPowerUsage(){
+        return MIN_POWER + (CPULoad/MAX_CPU) * (MAX_POWER - MIN_POWER);
+    }
 }
