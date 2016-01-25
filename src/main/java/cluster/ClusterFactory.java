347f356f5574307f4348381b54fb140bbe2d4ce9
Dennis Schroer <dev@dennisschroer.nl>
2016-01-25 17:46:08 +0100

    ClusterFactory; Debuglevel in Params

---
+++ src/main/java/cluster/ClusterFactory.java
@@ -0,0 +1,111 @@
+package cluster;
+
+import cluster.Cable;
+import cluster.Cluster;
+import cluster.Server;
+import cluster.World;
+import graph.Node;
+import simulation.Params;
+import switches.MainSwitch;
+import switches.Switch;
+import vm.M4LargeVM;
+import vm.M4XLargeVM;
+import vm.VM;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClusterFactory {
+
+    /**
+     * Creates a simple cluster with one switch and the given number of servers.
+     * No VMs are installed
+     */
+    public static Cluster<Node, Cable> simpleEmptyCluster(int servers) {
+        List<Node> nodes = new ArrayList<>();
+        List<Cable> edges = new ArrayList<>();
+
+        // Create world
+        World world = new World(0);
+
+        // Create switch
+        Switch switch1 = new MainSwitch(1);
+
+        // connect to world
+        nodes.add(switch1);
+        edges.add(createCable(world, switch1));
+
+        // Create servers
+        for(int j=0; j<servers; j++){
+            Server server = new Server(j+1);
+            nodes.add(server);
+            edges.add(createCable(server, switch1));
+        }
+
+        // Return the cluster
+        return new Cluster<>(world, nodes, edges);
+    }
+
+    /**
+     * Creates a simple cluster with one switch, two servers and one VM on both server.
+     */
+    public static Cluster<Node, Cable> simpleCluster() {
+        List<Node> nodes = new ArrayList<Node>();
+        List<Cable> edges = new ArrayList<Cable>();
+
+        // Create world
+        World world = new World(0);
+
+        Switch switch1 = new MainSwitch(1);
+
+        // Create servers
+        Server server1 = new Server(1);
+        Server server2 = new Server(2);
+
+        // Create VMs
+        VM vm1 = new M4LargeVM(1);
+        VM vm2 = new M4XLargeVM(2);
+        VM vm3 = new M4XLargeVM(3);
+        VM vm4 = new M4XLargeVM(4);
+        VM vm5 = new M4XLargeVM(5);
+
+        // Add VMs to server
+        server1.addVM(vm1);
+        server2.addVM(vm2);
+        server2.addVM(vm3);
+        server2.addVM(vm4);
+        server2.addVM(vm5);
+
+        // Add nodes
+        nodes.add(world);
+        nodes.add(switch1);
+        nodes.add(server1);
+        nodes.add(server2);
+
+        // Create edges
+        edges.add(createCable(server1, switch1));
+        edges.add(createCable(server2, switch1));
+        edges.add(createCable(switch1, world));
+
+        // Vms are linked
+        vm1.connectToVM(vm2);
+        vm2.connectToVM(vm1);
+
+        // Return the cluster
+        return new Cluster<Node, Cable>(world, nodes, edges);
+    }
+
+    /**
+     * Create a new cable between the two nodes.
+     *
+     * @param node1 The first node.
+     * @param node2 The second node.
+     * @return A Cable between the nodes.
+     */
+    public static Cable createCable(Node node1, Node node2) {
+        Cable cable = new Cable(node1, node2, Params.CABLE_CAPACITY);
+        node1.addEdge(cable);
+        node2.addEdge(cable);
+        return cable;
+    }
+}
--- src/main/java/simulation/ClusterSimulation.java
+++ src/main/java/simulation/ClusterSimulation.java
@@ -138,68 +138,6 @@ public class ClusterSimulation {
     }
 
 
-    /**
-     * Creates a simple cluster with one switch, two servers and one VM on both server.
-     */
-    public static Cluster<Node, Cable> simpleCluster() {
-        List<Node> nodes = new ArrayList<Node>();
-        List<Cable> edges = new ArrayList<Cable>();
-
-        // Create world
-        World world = new World(0);
-
-        Switch switch1 = new MainSwitch(1);
-
-        // Create servers
-        Server server1 = new Server(1);
-        Server server2 = new Server(2);
-
-        // Create VMs
-        VM vm1 = new M4LargeVM(1);
-        VM vm2 = new M4XLargeVM(2);
-        VM vm3 = new M4XLargeVM(3);
-        VM vm4 = new M4XLargeVM(4);
-        VM vm5 = new M4XLargeVM(5);
-
-        // Add VMs to server
-        server1.addVM(vm1);
-        server2.addVM(vm2);
-        server2.addVM(vm3);
-        server2.addVM(vm4);
-        server2.addVM(vm5);
-
-        // Add nodes
-        nodes.add(world);
-        nodes.add(switch1);
-        nodes.add(server1);
-        nodes.add(server2);
-
-        // Create edges
-        edges.add(createCable(server1, switch1));
-        edges.add(createCable(server2, switch1));
-        edges.add(createCable(switch1, world));
-
-        // Vms are linked
-        vm1.connectToVM(vm2);
-        vm2.connectToVM(vm1);
-
-        // Return the cluster
-        return new Cluster<Node, Cable>(world, nodes, edges);
-    }
-
-    /**
-     * Create a new cable between the two nodes.
-     *
-     * @param node1 The first node.
-     * @param node2 The second node.
-     * @return A Cable between the nodes.
-     */
-    private static Cable createCable(Node node1, Node node2) {
-        Cable cable = new Cable(node1, node2, Params.CABLE_CAPACITY);
-        node1.addEdge(cable);
-        node2.addEdge(cable);
-        return cable;
-    }
 
     public static void main(String[] args) {
         // Setup logging
@@ -221,11 +159,11 @@ public class ClusterSimulation {
     private static void setupLogging(){
         Logger globalLogger = LogManager.getLogManager().getLogger("");
         Handler handler = new ConsoleHandler();
-        globalLogger.setLevel(Level.INFO);
+        globalLogger.setLevel(Params.LOG_LEVEL);
         for(Handler defaultHandler : globalLogger.getHandlers()){
             globalLogger.removeHandler(defaultHandler);
         }
-        handler.setLevel(Level.INFO);
+        handler.setLevel(Params.LOG_LEVEL);
         handler.setFormatter(new ClusterSimLogFormatter());
         globalLogger.addHandler(handler);
     }
